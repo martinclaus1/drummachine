ce71dacd60931e6181c26d709b198f81adb8a4a2
Martin Claus <martinclaus@martinclaus.eu>
2022-01-29 17:48:02 +0100

    continue with mantine styling, add radix icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { AppShell, Header, Text, ThemeIcon } from '@mantine/core';
+import { PlayIcon, RocketIcon } from '@modulz/radix-icons';
 import React from 'react';
 import './App.scss';
 import DrumMachine from './DrumMachine';
 
 const App: React.FC = () => (
-  <div className="App">
+  <AppShell
+    header={
+      <Header height={60} style={{display: 'flex'}} padding="xs">
+        <ThemeIcon radius="lg">
+          <PlayIcon />
+        </ThemeIcon>
+        <Text style={{marginLeft: "1rem"}}>
+          Drum Computer
+        </Text>
+      </Header>
+    }
+  >
     <DrumMachine />
-  </div>
+  </AppShell>
 );
 
 export default App;
