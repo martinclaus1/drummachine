58ea69445bdd6da4c085cd3ab2b6447e6597b285
Martin Claus <martinclaus@posteo.de>
2022-01-30 17:18:55 +0100

    improve basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import { AppShell, Header, Text, ThemeIcon } from '@mantine/core';
-import { PlayIcon, RocketIcon } from '@modulz/radix-icons';
+import { AppShell, Header, MantineProvider, Text, ThemeIcon } from '@mantine/core';
+import { PlayIcon } from '@modulz/radix-icons';
 import React from 'react';
-import './App.scss';
 import DrumMachine from './DrumMachine';
 
 const App: React.FC = () => (
-  <AppShell
-    header={
-      <Header height={60} style={{display: 'flex'}} padding="xs">
-        <ThemeIcon radius="lg">
-          <PlayIcon />
-        </ThemeIcon>
-        <Text style={{marginLeft: "1rem"}}>
-          Drum Computer
-        </Text>
-      </Header>
-    }
-  >
-    <DrumMachine />
-  </AppShell>
+  <MantineProvider>
+    <AppShell
+      header={
+        <Header height='auto' style={{ display: 'flex' }} padding="xs">
+          <ThemeIcon radius="lg">
+            <PlayIcon />
+          </ThemeIcon>
+          <Text style={{ marginLeft: "1rem" }} weight='bold'>
+            Drum Computer
+          </Text>
+        </Header>
+      }
+    >
+      <DrumMachine />
+    </AppShell>
+  </MantineProvider >
 );
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import './globals'
+import './globals';
 
 ReactDOM.render(<App />, document.getElementById('root'));
