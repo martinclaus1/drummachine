e69a1d084cadbbcde612f9b6c1664ec5463b562a
Martin Claus <marc@spreadshirt.net>
2022-02-02 21:32:51 +0100

    cleanup soundplayer

--- src/SoundPlayer.ts
+++ src/SoundPlayer.ts
@@ -1,15 +1,6 @@
 import SamplePlayer from './SamplePlayer';
 
-const samples = [
-  'clap',
-  'cowbell',
-  'kick',
-  'snare',
-  'hihat',
-  'ride',
-  'rim'
-];
-
+const availableSamples = ['clap', 'cowbell', 'kick', 'snare', 'hihat', 'ride', 'rim'];
 export default class SoundPlayer {
   instruments: Record<string, SamplePlayer>;
   constructor() {
@@ -17,16 +8,15 @@ export default class SoundPlayer {
   }
 
   prepare = (context: AudioContext) => {
-    return Promise.all(samples.map(sample => this.loadSample(context, sample)))
-  }
+    return Promise.all(availableSamples.map((sample) => this.loadSample(context, sample)));
+  };
 
-  // rework to async await
   loadSample = async (context: AudioContext, instrument: string) => {
     const url = `./samples/${instrument}.wav`;
-    const data = await fetch(url).then(response => response.arrayBuffer());
+    const data = await fetch(url).then((response) => response.arrayBuffer());
     const decodedAudio = await context.decodeAudioData(data);
     this.instruments[instrument] = new SamplePlayer(decodedAudio);
-  }
+  };
 
   play(context: AudioContext, instrument: string, timing: number) {
     const player = this.instruments[instrument];
@@ -34,6 +24,8 @@ export default class SoundPlayer {
     gainNode.gain.setValueAtTime(0.1, timing);
     gainNode.connect(context.destination);
 
-    if (player) player.play(context, timing, gainNode);
+    if (player) {
+      player.play(context, timing, gainNode);
+    }
   }
 }
