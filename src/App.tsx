87838ad8397be7b965426413dc48509c859ff75f
Martin Claus <marc@spreadshirt.net>
2022-07-08 18:59:57 +0200

    add github icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import {AppShell, Header, MantineProvider, Text, ThemeIcon} from '@mantine/core';
-import {PlayIcon} from '@modulz/radix-icons';
+import {Anchor, AppShell, Header, MantineProvider, Text, ThemeIcon} from '@mantine/core';
+import {GitHubLogoIcon, PlayIcon} from '@modulz/radix-icons';
 import * as React from 'react';
 import DrumMachine from './DrumMachine';
 
-const App: React.FC = () => (
-    <MantineProvider>
-        <AppShell
-            header={
-                <Header height="auto" style={{display: 'flex'}} p="xs">
-                    <ThemeIcon radius="lg">
-                        <PlayIcon/>
-                    </ThemeIcon>
-                    <Text ml="xs" weight="bold">
-                        Drum Machine
-                    </Text>
-                </Header>
-            }
-        >
-            <DrumMachine/>
-        </AppShell>
-    </MantineProvider>
-);
+const App: React.FC = () => {
+    const header = <Header height={50} style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}} p="xs">
+        <div style={{display: 'flex', alignItems: 'center'}}>
+            <ThemeIcon radius="lg">
+                <PlayIcon/>
+            </ThemeIcon>
+            <Text ml="xs" weight="bold">
+                Drum Machine
+            </Text>
+        </div>
+        <div>
+            <Anchor href={"https://github.com/martinclaus1/drummachine"} target="_blank">
+                <ThemeIcon radius="sm" variant="outline">
+                    <GitHubLogoIcon />
+                </ThemeIcon>
+            </Anchor>
+        </div>
+    </Header>;
+
+    return (
+        <MantineProvider>
+            <AppShell header={header}>
+                <DrumMachine/>
+            </AppShell>
+        </MantineProvider>
+    );
+};
 
 export default App;
